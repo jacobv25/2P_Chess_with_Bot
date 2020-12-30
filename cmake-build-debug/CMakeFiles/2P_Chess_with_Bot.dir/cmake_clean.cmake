d5bb3c838451b295d42ec2e8422ced43eff5c51d
Jacob Valenzuela <32970534+jacobv25@users.noreply.github.com>
2020-12-29 18:12:44 -0800

    Deleted Square.h and Square.cpp

---
+++ cmake-build-debug/CMakeFiles/2P_Chess_with_Bot.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "2P_Chess_with_Bot.exe"
+  "2P_Chess_with_Bot.exe.manifest"
+  "2P_Chess_with_Bot.pdb"
+  "CMakeFiles/2P_Chess_with_Bot.dir/Board.cpp.obj"
+  "CMakeFiles/2P_Chess_with_Bot.dir/Driver.cpp.obj"
+  "lib2P_Chess_with_Bot.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2P_Chess_with_Bot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
